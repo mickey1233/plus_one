7cc82d46050a0bb44adb1fb1323bd7af8d20fee1
mickey1233 <42233582+mickey1233@users.noreply.github.com>
2021-09-06 01:29:38 +0800

    Update plus_one.py

--- plus_one.py
+++ plus_one.py
@@ -5,17 +5,28 @@ def plusone(digits):
         sum = sum + (i * di)
         di = di * 10
     sum = sum + 1
-    return list(map(int, str(sum)))
+    return (list(map(int, str(sum))))
+
+
+def plusone1(digits):
+    return list(map(int, str((int("".join(map(str, digits)))+1))))
 
 
 def main():
     x = []
+    y = []
     x.append(plusone([1, 2, 3]))
+    y.append(plusone([1, 2, 3]))
     x.append(plusone([4, 3, 2, 1]))
+    y.append(plusone1([4, 3, 2, 1]))
     x.append(plusone([0]))
+    y.append(plusone([0]))
     x.append(plusone([9]))
+    y.append(plusone([9]))
     print(x)
+    print(y)
 
 
 if __name__ == "__main__":
     main()
+
